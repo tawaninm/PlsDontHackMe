2e0ac32f62cb483b9b9508206aedcdfd4dd869fa
Leo <tmanusnon@gmail.com>
2025-08-27 23:04:40 +0700

    Revert "Working Multiple Cards"
    This reverts commit 5d03fa0761b03e36fe314e5e326e5f3c86a5c40d.

--- Script/CardData.gd
+++ Script/CardData.gd
@@ -1,8 +1,16 @@
 extends Node
 
 const CARDS = {
-	"Cat": [],
-	"CorruptedScript": [],
-	"VirusAttack": [],
-	"Overclock": [],
+	"Cat": {
+		"artwork": preload("res://Asset/CARDS/13.png"),
+	},
+	"Overclock": {
+		"artwork": preload("res://Asset/CARDS/6.png"),
+	},
+	"CorruptedScript": {
+		"artwork": preload("res://Asset/CARDS/9.png"),
+	},
+	"VirusAttack": {
+		"artwork": preload("res://Asset/CARDS/19.png"),
+	},
 }
--- Script/deck.gd
+++ Script/deck.gd
@@ -2,32 +2,25 @@ extends Node2D
 
 const CARD_SCENE_PATH = "res://Scene/card.tscn"
 var card_types = ["Overclock", "CorruptedScript", "VirusAttack", "Cat"]
-var card_data_reference
-var card_manager_reference
-
-func _ready() -> void:
-	card_data_reference = preload("res://Script/CardData.gd")
-	card_manager_reference = $"../CardManager"
+var card_data_reference = preload("res://Script/CardData.gd")
 
 func draw_card():
 	print("draw")
 
-	# Pick a random card type.
+	# Pick random type
 	var card_type = card_types[randi() % card_types.size()]
 
-	# Create a new card instance.
+	# Make new card
 	var card_scene = preload(CARD_SCENE_PATH)
 	var new_card = card_scene.instantiate()
-	
-	# Add the new card to the CardManager as a child.
-	card_manager_reference.add_child(new_card)
+	$"../CardManager".add_child(new_card)
 
-	# Corrected path for the card texture.
-	var card_image_page = str("res://Asset/CARDS/"+card_type+".png")
-	new_card.get_node("Area2D/CardImage").texture = load(card_image_page)
-
-	# Name it.
+	# Name it
 	new_card.name = "Card_" + card_type
 
-	# Send to hand.
+	# Apply artwork (⚠ requires the card scene to have a Sprite2D or TextureRect)
+	if new_card.has_node("Artwork"): # your child node inside card.tscn
+		new_card.get_node("Artwork").texture = card_data_reference.CARDS[card_type]["artwork"]
+
+	# Send to hand
 	$"../PlayerHand".add_card_to_hand(new_card)
