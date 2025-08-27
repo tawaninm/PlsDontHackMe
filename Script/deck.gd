02b6a691fd9cbabd045d4bdb0c4765c237a052a3
Leo <tmanusnon@gmail.com>
2025-08-27 23:06:41 +0700

    Reapply "Working Multiple Cards"
    This reverts commit 2e0ac32f62cb483b9b9508206aedcdfd4dd869fa.

--- Script/deck.gd
+++ Script/deck.gd
@@ -2,25 +2,32 @@ extends Node2D
 
 const CARD_SCENE_PATH = "res://Scene/card.tscn"
 var card_types = ["Overclock", "CorruptedScript", "VirusAttack", "Cat"]
-var card_data_reference = preload("res://Script/CardData.gd")
+var card_data_reference
+var card_manager_reference
+
+func _ready() -> void:
+	card_data_reference = preload("res://Script/CardData.gd")
+	card_manager_reference = $"../CardManager"
 
 func draw_card():
 	print("draw")
 
-	# Pick random type
+	# Pick a random card type.
 	var card_type = card_types[randi() % card_types.size()]
 
-	# Make new card
+	# Create a new card instance.
 	var card_scene = preload(CARD_SCENE_PATH)
 	var new_card = card_scene.instantiate()
-	$"../CardManager".add_child(new_card)
+	
+	# Add the new card to the CardManager as a child.
+	card_manager_reference.add_child(new_card)
 
-	# Name it
+	# Corrected path for the card texture.
+	var card_image_page = str("res://Asset/CARDS/"+card_type+".png")
+	new_card.get_node("Area2D/CardImage").texture = load(card_image_page)
+
+	# Name it.
 	new_card.name = "Card_" + card_type
 
-	# Apply artwork (⚠ requires the card scene to have a Sprite2D or TextureRect)
-	if new_card.has_node("Artwork"): # your child node inside card.tscn
-		new_card.get_node("Artwork").texture = card_data_reference.CARDS[card_type]["artwork"]
-
-	# Send to hand
+	# Send to hand.
 	$"../PlayerHand".add_card_to_hand(new_card)
