38def3d1d3322128f464b5948eab2b0a43e450ec
Thanatpat Promthpong <tawaninm13@gmail.com>
2025-09-06 20:47:58 +0700

    card Filp Spawn card match asset point

--- Script/deck.gd
+++ Script/deck.gd
@@ -9,13 +9,14 @@ var card_database_reference
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	player_deck.shuffle()
 	$RichTextLabel.text = str(player_deck.size())
 	card_database_reference = preload("res://Script/CardDatabase.gd")
 
 
 func draw_card():
-	var card_drawn = player_deck[0]
-	player_deck.erase(card_drawn)
+	var card_drawn_name = player_deck[0]
+	player_deck.erase(card_drawn_name)
 	
 	if player_deck.size() == 0:
 		$Area2D/CollisionShape2D.disabled = true
@@ -25,8 +26,11 @@ func draw_card():
 	$RichTextLabel.text = str(player_deck.size())
 	var card_scene = preload(CARD_SCENE_PATH)
 	var new_card = card_scene.instantiate()
-	new_card.get_node("CPU").text = str(card_database_reference.CARDS[card_drawn][0])
-	new_card.get_node("Ingre").text = str(card_database_reference.CARDS[card_drawn][1])
+	var card_image_path = str("res://Asset/IMG/" + card_drawn_name + "Card.png")
+	new_card.get_node("CardImg").texture = load(card_image_path)
+	new_card.get_node("CPU").text = str(card_database_reference.CARDS[card_drawn_name][0])
+	new_card.get_node("Ingre").text = str(card_database_reference.CARDS[card_drawn_name][1])
 	$"../CardManager".add_child(new_card)
 	new_card.name = "card"
 	$"../Playerhand".add_card_to_hand(new_card, CARD_DRAW_SPEED)
+	new_card.get_node("AnimationPlayer").play("card_filp")
